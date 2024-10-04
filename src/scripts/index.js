5546922138b8af82550ccddae42d61b9e611f532
Stroick <erik_o_mueller@yahoo.com>
2024-10-04 04:11:36 -0500

    feat: add more oneNote functionalities.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,19 +2,29 @@ import "../index.css";
 import "../header.css"; 
 import "../pageDisplay.css"; 
 import "../notebookDropdown.css"; 
+import "../dropdown.css"; 
 import "../main.css"; 
 import { state } from "./state";
 
 const body = document.querySelector(`body`);
 
-body.addEventListener(`click`, (e) => {
+body.addEventListener(`click`, (e) => { // TODO: click event to change section color. After that I think I should move on with Odin Project. Because that will basically be the todo app. Can keep working on it though.
+    if(state.sectionRenamingElement && !(e.target.classList.contains(`sectionName`) && e.target.dataset.index === state.sectionRenamingElement.dataset.index)) { // ! Needs to be placed near top.
+        state.stopSectionRenaming();
+    }
+    if(e.target.classList.contains(`dropdownRename`)) {
+        state.renameSection();
+    }
+    if(e.target.classList.contains(`dropdownDelete`)) {
+        state.deleteSection();
+    }
     if(e.target.classList.contains(`pages`)) {
         state.openPage(e.target);
     }
     if(e.target.classList.contains(`addPageButton`)) {
         state.createNewPage();
     }
-    if(e.target.classList.contains(`section`) && !e.target.classList.contains(`add`) && !e.target.classList.contains(`open`)) {
+    if( ( e.target.classList.contains(`section`) || e.target.classList.contains(`sectionName`) ) && !e.target.classList.contains(`add`) && !e.target.classList.contains(`open`)) {
         state.openSection(e.target);
     }
     if(e.target.classList.contains(`section`) && e.target.classList.contains(`add`)) {
@@ -35,7 +45,7 @@ body.addEventListener(`click`, (e) => {
     if(e.target.classList.contains(`closeAddNotebookModalBtn`)) {
         state.hideAddNotebookModal();
     }
-    if(e.target.classList.contains(`switchNotebook`)) { // ! Beware: the "else" here hides all dropdowns.
+    if(e.target.classList.contains(`switchNotebook`)) { // ! Beware: the "else" here hides all dropdowns. Needs to be placed lower.
         if(state.currentDropdownEle) {
             state.hideAllDropdowns();
         } else {
@@ -43,5 +53,47 @@ body.addEventListener(`click`, (e) => {
         }
     } else {
         state.hideAllDropdowns();
+    }
+});
+
+body.addEventListener(`input`, (e) => {
+    if(e.target.classList.contains(`pageTitle`)) {
+        state.updatePageTitle(e.target);
+    }
+    if(e.target.classList.contains(`pageContent`)) {
+        state.updatePageContent(e.target);
+    }
+});
+
+body.addEventListener(`contextmenu`, (e) => {
+    e.preventDefault();
+    if(e.target.classList.contains(`section`) || e.target.classList.contains(`sectionName`) && !e.target.classList.contains(`add`)) {
+        if(state.currentDropdownEle) {
+            state.hideAllDropdowns();
+            state.showSectionDropdown(e);
+        } else {
+            state.showSectionDropdown(e);
+        }
+    }
+});
+
+body.addEventListener(`dblclick`, (e) => {
+    if(e.target.classList.contains(`sectionName`)) {
+        state.renameSection(e.target);
+    }
+});
+
+body.addEventListener(`keypress`, (e) => {
+    if(e.key === `Enter` && state.sectionRenamingElement) {
+        state.stopSectionRenaming();
+    }
+});
+
+body.addEventListener(`mouseover`, (e) => {
+    if(e.target.classList.contains(`openColorDropdown`)) {
+        state.showColorDropdown();
+    }
+    if(e.target.classList.contains(`dropdown`) && !(e.target.classList.contains(`openColorDropdown`) || e.target.classList.contains(`colorDropdownContainer`) || e.target.classList.contains(`color`))) {
+        state.hideColorDropdown();
     }
 });
--- src/scripts/state.js
+++ src/scripts/state.js
@@ -10,10 +10,19 @@ const state = {
     currentlyOpenNBNameDisplay: document.querySelector(`.notebookName.switchNotebook`),
     sectionSelectContainer: document.querySelector(`.sectionSelect`),
     pageListContainer: document.querySelector(`.pageList`),
+    pageDisplayContainer: document.querySelector(`.pageDisplay`),
+    dropdownContainer: document.querySelector(`.dropdownContainer`),
+    colorDropdownContainer: document.querySelector(`.colorDropdownContainer`),
 
     currentlyOpenNotebook: null,
 
     currentDropdownEle: null,
+    currentDropdownTargetEle: null,
+    currentDropdownEle2: null,
+
+    createNewSectionRenameEle: null, // ! Workaround for the sectionRename not working when I try to call it with createNewSection().
+
+    sectionRenamingElement: null,
 
     notebookArr: [],
 
@@ -34,7 +43,7 @@ const state = {
     Page: function (index) {
         this.index = index;
         this.name = `Untitled`;
-        this.date = ``;
+        this.date = `Tuesday, October 1, 2024&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4:46 PM`;
         this.content = ``;
     },
     createNotebook: (name) => {
@@ -74,6 +83,10 @@ const state = {
         if(state.currentDropdownEle) {
             state.currentDropdownEle.style.display = `none`;
             state.currentDropdownEle = null;
+            if(state.currentDropdownEle2) {
+                state.currentDropdownEle2.style.display = `none`;
+                state.currentDropdownEle2 = null;
+            }
         }
     },
     showAddNotebookModal: () => {
@@ -99,18 +112,25 @@ const state = {
         state.updateCurrentlyOpenNBNameDisplay();
         state.updateSectionSelectContainer();
         state.updatePageList();
-},
+        state.updatePageDisplay();
+    },
     updateSectionSelectContainer: () => {
         state.sectionSelectContainer.innerHTML = ``;
         state.currentlyOpenNotebook.sections.forEach(ele => {
             let section = document.createElement(`div`);
+            let sectionName = document.createElement(`div`);
             if(state.currentlyOpenNotebook.openSection === ele) {
-                section.className = `section open`;
+                section.className = `section open`
+                sectionName.className = `sectionName open`
             } else {
                 section.className = `section`;
+                sectionName.className = `sectionName`
             }
-            section.textContent = ele.name;
             section.dataset.index = ele.index;
+            
+            sectionName.textContent = ele.name;
+            sectionName.dataset.index = ele.index;
+            section.append(sectionName);
             state.sectionSelectContainer.append(section);
         });
         let section = document.createElement(`div`);
@@ -121,33 +141,162 @@ const state = {
     createNewSection: () => {
         state.createSection(state.currentlyOpenNotebook, `New Section 1`);
         state.updateSectionSelectContainer();
+        state.updatePageDisplay();
+        let lastSectionSelect = document.querySelector(`.sectionSelect .section:nth-last-child(2)`);
+        state.createNewSectionRenameEle = document.querySelector(`.sectionSelect .section:nth-last-child(2) .sectionName`); // ! workaround mentioned in the global var's of state obj.
+        state.openSection(lastSectionSelect);
+        state.renameSection();
     },
     openSection: (clickTarget) => {
         state.currentlyOpenNotebook.openSection = state.currentlyOpenNotebook.sections[clickTarget.dataset.index];
         state.updateSectionSelectContainer();
         state.updatePageList();
+        state.updatePageDisplay();
     },
     updatePageList: () => {
         state.pageListContainer.innerHTML = ``;
         state.currentlyOpenNotebook.openSection.pages.forEach(ele => {
             let page = document.createElement(`div`);
-            if(state.currentlyOpenNotebook.openSection.openPage === ele) {
-                page.className = `pages open`;
+            state.currentlyOpenNotebook.openSection.openPage === ele ? page.className = `pages open` : page.className = `pages`;
+            if(ele.name.length > 29) {
+                let textArray = ele.name.split(``);
+                textArray.splice(30, Infinity, `.`, `.`, `.`);
+                let joinedtextArray = textArray.join(``);
+                page.textContent = joinedtextArray;
+            } else if(ele.name.length < 1) {
+                page.textContent = `Untitled`;
             } else {
-                page.className = `pages`;
+                page.textContent = ele.name;
             }
-            page.textContent = ele.name;
             page.dataset.index = ele.index;
             state.pageListContainer.append(page);
         });
     },
-    createNewPage: () => {
+    createNewPage: () => { // TODO: Make this open the page that was created.
         state.createPage(state.currentlyOpenNotebook.openSection);
         state.updatePageList();
+        state.updatePageDisplay();
     },
-    openPage: (clickTarget) => {
+    openPage: (clickTarget) => { // TODO: make this focus where the cursor left off.
         state.currentlyOpenNotebook.openSection.openPage = state.currentlyOpenNotebook.openSection.pages[clickTarget.dataset.index];
         state.updatePageList();
+        state.updatePageDisplay();
+    },
+    updatePageDisplay: () => {
+        state.pageDisplayContainer.innerHTML = ``;
+        let page = document.createElement(`div`);
+        page.className = `page`;
+        let pageInfo = document.createElement(`div`);
+        pageInfo.className = `pageInfo`;
+        page.append(pageInfo);
+        let pageTitle = document.createElement(`div`);
+        pageTitle.className = `pageTitle`;
+        pageTitle.contentEditable = true;
+        pageTitle.textContent = state.currentlyOpenNotebook.openSection.openPage.name;
+        pageInfo.append(pageTitle);
+        let pageInfoLine = document.createElement(`div`);
+        pageInfoLine.className = `pageInfoLine`;
+        pageInfo.append(pageInfoLine);
+        let pageDate = document.createElement(`div`);
+        pageDate.innerHTML = state.currentlyOpenNotebook.openSection.openPage.date;
+        pageDate.className = `pageDate`;
+        pageInfo.append(pageDate);
+        let pageContent = document.createElement(`div`);
+        pageContent.className = `pageContent`;
+        pageContent.contentEditable = true;
+        pageContent.textContent = state.currentlyOpenNotebook.openSection.openPage.content;
+        page.append(pageContent);
+        state.pageDisplayContainer.append(page);
+    },
+    updatePageTitle: (inputTarget) => {
+        state.currentlyOpenNotebook.openSection.openPage.name = inputTarget.textContent;
+        state.updatePageList();
+    },
+    updatePageContent: (inputTarget) => {
+        state.currentlyOpenNotebook.openSection.openPage.content = inputTarget.textContent;
+    },
+    showSectionDropdown: (e) => {
+        state.dropdownContainer.style.display = `flex`;
+
+        if(e.y + state.dropdownContainer.offsetHeight > window.innerHeight) {
+            state.dropdownContainer.style.top = `${window.innerHeight - state.dropdownContainer.offsetHeight}px`;
+        } else {
+            state.dropdownContainer.style.top = `${e.y}px`;
+        }
+
+        if(e.x + state.dropdownContainer.offsetWidth > window.innerWidth) {
+            state.dropdownContainer.style.left = `${window.innerWidth - state.dropdownContainer.innerWidth}px`;
+        } else {
+            state.dropdownContainer.style.left = `${e.x}px`;
+        }
+
+        state.currentDropdownTargetEle = e.target;
+        state.currentDropdownEle = state.dropdownContainer;
+    },
+    renameSection: (dblclickTarget) => {
+        if(dblclickTarget) {
+            dblclickTarget.contentEditable = true;
+            dblclickTarget.focus();
+            state.sectionRenamingElement = dblclickTarget;
+        } else if (state.createNewSectionRenameEle) {
+            let renameTarget = document.querySelector(`.sectionName[data-index="${state.createNewSectionRenameEle.dataset.index}"]`);
+            renameTarget.contentEditable = true;
+            renameTarget.focus();
+            state.sectionRenamingElement = renameTarget;
+            state.createNewSectionRenameEle = null;
+        } else {
+            let renameTarget = document.querySelector(`.sectionName[data-index="${state.currentDropdownTargetEle.dataset.index}"]`);
+            renameTarget.contentEditable = true;
+            renameTarget.focus();
+            state.sectionRenamingElement = renameTarget;
+        }
+        
+    },
+    stopSectionRenaming: () => {
+        state.sectionRenamingElement.contentEditable = false;
+        state.currentlyOpenNotebook.sections[state.sectionRenamingElement.dataset.index].name = state.sectionRenamingElement.textContent;
+        state.sectionRenamingElement = null;
+    },
+    deleteSection: () => {
+        const updateAllSectionIndicies = () => {
+            for(let i = 0; i < state.currentlyOpenNotebook.sections.length; i++) {
+                state.currentlyOpenNotebook.sections[i].index = i;
+            }
+        }
+        state.currentlyOpenNotebook.sections.splice(state.currentDropdownTargetEle.dataset.index, 1);
+        updateAllSectionIndicies(); // * when removing an element from the array, the nested ".index" properties of all of the sections need to be reset.
+
+        let nextSectionToSelect = document.querySelector(`.sectionSelect .section:nth-last-child(3)`);
+        console.log(nextSectionToSelect)
+        state.openSection(nextSectionToSelect);
+        state.updateSectionSelectContainer();
+        state.updatePageList();
+        state.updatePageDisplay();
+    },
+    showColorDropdown: () => {
+        let parentDropdownContainer = state.currentDropdownEle;
+        state.colorDropdownContainer.style.display = `flex`;
+
+        // if(e.y + state.colorDropdownContainer.offsetHeight > window.innerHeight) {
+            state.colorDropdownContainer.style.top = `${parentDropdownContainer.offsetTop}px`;
+        // } else {
+            // state.colorDropdownContainer.style.top = `${e.y}px`;
+        // }
+
+        // if(e.x + state.colorDropdownContainer.offsetWidth > window.innerWidth) {
+            state.colorDropdownContainer.style.left = `${parentDropdownContainer.offsetWidth + parentDropdownContainer.offsetLeft}px`;
+        // } else {
+            // state.colorDropdownContainer.style.left = `${e.x}px`;
+        // }
+
+        // state.currentDropdownTargetEle = e.target;
+        state.currentDropdownEle2 = state.colorDropdownContainer;
+    },
+    hideColorDropdown: () => {
+        if(state.currentDropdownEle2) {
+            state.currentDropdownEle2.style.display = `none`;
+            state.currentDropdownEle2 = null;
+        }
     },
 }
 
@@ -156,6 +305,7 @@ state.currentlyOpenNotebook = state.notebookArr[0];
 state.updateCurrentlyOpenNBNameDisplay();
 state.updateSectionSelectContainer();
 state.updatePageList();
+state.updatePageDisplay();
 
 console.log(state.notebookArr);
 
